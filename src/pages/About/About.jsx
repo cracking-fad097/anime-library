84cf0c1a54685c941a6c6ea5a4ae768bf7701131
VsevolodMogutnenko <vmogutnenko@gmail.com>
2026-08-30 11:14:50 +0200

    updateSevalodka

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,16 @@
+import styles from './About.module.css'
+import developersArray from '../../data/developers.json'
+
 export const About = () => {
-    return <>
-        About
-    </>
+    return <div className={styles.about}>
+        <h1>⛩️Meet Our Developers 👋</h1>
+        <ul className={styles.developersWrapper}>
+            {developersArray.map(({id, img, name, age, github}) => <li className={styles.developers} key={id}>
+                <img src={img} alt=".." className={styles.developersImg} />
+                <p className={styles.developersInfo}>{name}</p>
+                <p className={styles.developersInfo}>{age}</p>
+                <a href={github} target='_blank' className={styles.developersLink}>GitHub Link</a>
+            </li>)}
+        </ul>
+    </div>
 }
