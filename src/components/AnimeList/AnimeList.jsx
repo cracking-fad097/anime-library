b767f6fbe6d9bb78316fa6b16f5002297c2698c3
cracking-fad097 <crackingfad@gmail.com>
2026-09-09 18:38:49 +0200

    update

--- src/components/AnimeList/AnimeList.jsx
+++ src/components/AnimeList/AnimeList.jsx
@@ -5,6 +5,5 @@ export const AnimeList = ({animes}) => {
     return <ul className={styles.animeList}>
             {animes?.map(({id, attributes:{canonicalTitle, averageRating, posterImage:{small}}}) =>
             <AnimeItem key={id} id={id} title={canonicalTitle} vote={averageRating} poster={small} />)}
-            
         </ul>
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -12,20 +12,17 @@ export const SearchBar = () => {
     const handleInputChange = (event) => {
         const {value} = event.target
         setName(value)
+        setSearchParams({name:value})
     }
-    
+
     const handleSearchSubmit = (event) => {
         event.preventDefault()
-        setSearchParams({name})
-        // getByName(name).then((data) => setAnime(data))
+        setName(value)
+        setSearchParams({name:value})
     }
 
     return <form className={styles.wrapper} onSubmit={handleSearchSubmit}>
-        <input type="text" value={name} className={styles.input} onChange={handleInputChange}/>
+        <input type="text" className={styles.input} onChange={handleInputChange}/>
         <button type="submit" className={styles.searchIconWrapper}><FaSearch className={styles.searchIcon}/></button>
     </form>
 }
-
-/* TODO:
-    - otrisovat animeshki po imeni
-*/
