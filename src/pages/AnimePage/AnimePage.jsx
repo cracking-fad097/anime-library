551de6cf50798695679020370fb47e8d94c8e9b5
VsevolodMogutnenko <vmogutnenko@gmail.com>
2026-09-12 10:57:17 +0200

    update

--- src/pages/AnimePage/AnimePage.jsx
+++ src/pages/AnimePage/AnimePage.jsx
@@ -2,19 +2,24 @@ import { useState, useEffect, } from 'react'
 import { useParams } from 'react-router-dom'
 import { getById } from '../../helpers/services'
 import styles from './AnimePage.module.css'
+import error from '../../assets/images/error.jpg'
 
 export const AnimePage = () => {
     const {id} = useParams()
     const [anime, setAnime] = useState(null)
+    const [backgroundImg, setBackgroundImg] = useState(null)
     useEffect (() => {
-        getById(id).then((data) => {setAnime(data)})
+        getById(id).then((data) => {setAnime(data)
+            setBackgroundImg(data?.data.data.attributes.coverImage?.small ?? error)
+        })
     }, [])
+    console.log(backgroundImg)
     const path = anime?.data.data.attributes
 
     return <div className={styles.animeInfo} style={{
         backgroundImage: `
             linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
-            url(${path?.coverImage.small})`}}>
+            url(${backgroundImg})`}}>
             <div className={styles.animeWrapper}>
                 <h1 className={styles.animeName}>🔥 {path?.canonicalTitle} 🔥</h1>
                 <p>Age Rating: {path?.ageRatingGuide}</p>
@@ -30,6 +35,6 @@ export const AnimePage = () => {
 /*
 
 TODO:
-    - coverImage.small ternarnik
+
 
 */
